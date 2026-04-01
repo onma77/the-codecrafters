21ef925e7ab69ec53ea7bc0cc165f1ad1c312f55
onma77 <oigogapeace@gmail.com>
2026-04-01 15:30:01 +0100

    base.go

---
+++ sentinel-reborn/base.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func ConvToHex(num string) string {
+	result, err := strconv.ParseInt(num, 16, 64)
+	if err != nil {
+		return "Invalid hex"
+	}
+	return strconv.FormatInt(result, 10)
+}
+func ConvToBin(num string) string {
+	result, err := strconv.ParseInt(num, 2, 64)
+	if err != nil {
+		return "Invalid bin"
+	}
+	return strconv.FormatInt(result, 10)
+}
+
+func strConvert(text string) string {
+	input := strings.Fields(text)
+	for i := 0; i < len(input); i++ {
+		if input[i] == "(hex)" {
+			input[i-1] = ConvToHex(input[i-1])
+			input = append(input[:i], input[i+1:]...)
+		}
+
+		if input[i] == "(bin)" {
+			input[i-1] = ConvToBin(input[i-1])
+			input = append(input[:i], input[i+1:]...)
+		}
+
+	}
+	return strings.Join(input, " ")
+}
