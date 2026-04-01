7d0fb7060c6131c7e05120bcf02bf2607d85bc80
onma77 <oigogapeace@gmail.com>
2026-04-01 18:31:22 +0100

    goreloaded

--- sentinel-reborn/punctuation.go
+++ sentinel-reborn/punctuation.go
@@ -8,8 +8,10 @@ import (
 func fixPunctuation(text string) string {
 
 	p1 := regexp.MustCompile(`\s+([?.,:;!]+)`)
-
-	text = p1.ReplaceAllString(text, "$1 ")
+	p2 := regexp.MustCompile(`([?.,:;!]+)\s+`)
+	
+	text = p1.ReplaceAllString(text, "$1")
+	text = p2.ReplaceAllString(text, "$1 ")
 	result := strings.Fields(text)
 	return strings.Join(result, " ")
 }
--- sentinel-reborn/quote.go
+++ sentinel-reborn/quote.go
@@ -5,10 +5,11 @@ import (
 )
 
 func fixQuotes(text string) string {
-	p1 := regexp.MustCompile(`'\s+`)
-	p2 := regexp.MustCompile(`\s+'`)
+	p1 := regexp.MustCompile(`\s+'`)
+	p2 := regexp.MustCompile(`'\s`)
 
 	text = p1.ReplaceAllString(text, "'")
 	text = p2.ReplaceAllString(text, "'")
+
 	return text
 }
