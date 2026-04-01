371b948d0d7f3f5677f0a106a500263875f89b3d
Freedom-Celebrate <celebrateowulo@gmail.com>
2026-04-01 16:27:47 +0100

    fixArticles

--- sentinel-reborn/fixArticle.go
+++ sentinel-reborn/fixArticle.go
@@ -1,1 +1,27 @@
+package main
 
+import (
+	"strings"
+)
+
+func fixarticles(s string) string {
+	words := strings.Fields(s)
+	vowels := "aeiouhAEIOUH"
+
+	for i := 0; i < len(words); i++ {
+		if i < len(words) {
+			word := words[i]
+			if word == "a" && strings.ContainsAny(vowels, string(string(words[i+1][0]))) {
+				words[i] = "an"
+			} else if word == "A" && strings.ContainsAny(vowels, string(string(words[i+1][0]))) {
+				words[i] = "An"
+			} else if word == "an" && !strings.ContainsAny(vowels, string(string(words[i+1][0]))) {
+				words[i] = "a"
+			}
+		}
+
+	}
+
+	return strings.Join(words, " ")
+
+}
