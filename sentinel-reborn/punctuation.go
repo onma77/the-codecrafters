4d095589fdb2d1d201a9f6f25072023a64835e30
onma77 <oigogapeace@gmail.com>
2026-04-01 18:51:58 +0100

    goreloaded

--- sentinel-reborn/punctuation.go
+++ sentinel-reborn/punctuation.go
@@ -10,7 +10,7 @@ func fixPunctuation(text string) string {
 	p1 := regexp.MustCompile(`\s+([?.,:;!]+)`)
 	p2 := regexp.MustCompile(`([?.,:;!]+)\s+`)
 	
-	text = p1.ReplaceAllString(text, "$1")
+	text = p1.ReplaceAllString(text, "$1 ")
 	text = p2.ReplaceAllString(text, "$1 ")
 	result := strings.Fields(text)
 	return strings.Join(result, " ")
