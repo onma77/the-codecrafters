28201a4a9c6214a52594e34d35ea8dff61ae0947
Freedom-Celebrate <freedomcelebrate01@gmail.com>
2026-04-01 13:21:36 +0100

    Update article.go

---
+++ sentinel-reborn/functions/article.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"strings"
+)
+
+func fixarticles(s string) string {
+	words := strings.Fields(s)
+	vowels := "aeiouhAEIOUH"
+
+	for i := 0; i < len(words); i++ {
+		if i < len(words) {
+			word := words[i]
+			if word == "a" && strings.ContainsAny(vowels, string(string(words[i+1][0]))) {
+				words[i] = "an"
+			} else if word == "A" && strings.ContainsAny(vowels, string(string(words[i+1][0]))) {
+				words[i] = "An"
+			}
+
+		}
+	}
+	return strings.Join(words, " ")
+
+}
