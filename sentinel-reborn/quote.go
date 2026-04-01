a971c8af6485faf6c036060748c01d9fd466a2b6
KvngRagnar <emaikwungbede@gmail.com>
2026-04-01 16:18:39 +0100

    fully functional quote logic

--- sentinel-reborn/quote.go
+++ sentinel-reborn/quote.go
@@ -1,1 +1,14 @@
+package main
 
+import (
+	"regexp"
+)
+
+func fixQuotes(text string) string {
+	p1 := regexp.MustCompile(`'\s+`)
+	p2 := regexp.MustCompile(`\s+'`)
+
+	text = p1.ReplaceAllString(text, "'")
+	text = p2.ReplaceAllString(text, "'")
+	return text
+}
